82bd44a22f3c1d43ab39ddab619cfa35326f96ca
JIGRRA <jigarchovatiya12345@gmail.com>
2026-01-16 18:43:41 +0530

    extract zip

--- InvoiceViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace InvoiceApplication.Models
-{
-    public class InvoiceViewModel
-    {
-        public Invoice_Table Invoice { get; set; }
-        public List<InvoiceProduct> Products { get; set; }
-    }
-}
