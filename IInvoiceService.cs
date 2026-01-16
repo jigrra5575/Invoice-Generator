1b3cdc999fa2d0a4b207ae0b0b4401b7a449852b
JIGRRA <jigarchovatiya12345@gmail.com>
2026-01-16 16:46:28 +0530

    /Service

---
+++ IInvoiceService.cs
@@ -0,0 +1,17 @@
+﻿using InvoiceApplication.Models;
+using Microsoft.Data.SqlClient;
+
+namespace InvoiceApplication.Service
+{
+    public interface IInvoiceService
+    {
+       public List<Invoice_Table> GetInvoices();
+        //public void CreateInvoice(Invoice_Table invoice);
+        public void CreateInvoice(Invoice_Table invoice, List<InvoiceProduct> products);
+        public Invoice_Table GetById(int id);
+        public void EditInvoice(Invoice_Table invoice, List<InvoiceProduct> products);
+        public void deleteInvoice(int id);
+        public void ErrorHandaling(Exception error);
+        public void ErrorHandaling(SqlException error);
+    }
+}
