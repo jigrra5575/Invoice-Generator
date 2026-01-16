f9e01f1f3413937c3f0ffb213ff236380d1990b0
JIGRRA <jigarchovatiya12345@gmail.com>
2026-01-16 16:45:50 +0530

    /Models

---
+++ InvoiceProduct.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceApplication.Models
+{
+    public class InvoiceProduct
+    {
+        [Key]
+        public int Detail_Id { get; set; }
+        public int InvoiceNumber { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public int? Qty { get; set; }
+        public decimal? Total { get; set; }
+
+    }
+}
