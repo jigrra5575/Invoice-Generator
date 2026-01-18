b4b0301273903082550026a6058066d2ab6949b9
JIGRRA <jigarchovatiya12345@gmail.com>
2026-01-18 10:41:57 +0530

    Add files via upload

---
+++ Invoice-Generator/Models/InvoiceViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InvoiceApplication.Models
+{
+    public class InvoiceViewModel
+    {
+        public Invoice_Table Invoice { get; set; }
+        public List<InvoiceProduct> Products { get; set; }
+    }
+}
