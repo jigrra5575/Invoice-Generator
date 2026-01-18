856b6981797fb904d1452a75412be8a360356a83
JIGRRA <jigarchovatiya12345@gmail.com>
2026-01-18 10:40:07 +0530

    Add files via upload

--- Invoice-Generator/Database/myDbContext.cs
+++ Invoice-Generator/Database/myDbContext.cs
@@ -1,1 +1,20 @@
+﻿using InvoiceApplication.Models;
+using Microsoft.EntityFrameworkCore;
 
+namespace InvoiceApplication.Database
+{
+    public class myDbContext : DbContext
+    {
+        public myDbContext()
+        {
+        }
+
+        public myDbContext(DbContextOptions options) : base (options){ }
+
+        public DbSet<Invoice_Table> T_Invoice{ get; set; }
+        public DbSet<Invoice_Detail> Invoice_Detail{ get; set; }
+        public DbSet<InvoiceProduct> InvoiceProducts { get; set; }
+        public DbSet<ErrorHandaling> ErrorHandaling { get; set; }
+
+    }
+}
